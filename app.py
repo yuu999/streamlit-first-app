566b6bdf3e6ca69b0e7215d14e1e268082299dc5
yuu999 <yuu999.game@gmail.com>
2021-12-22 23:37:31 +0900

    update

--- app.py
+++ app.py
@@ -4,22 +4,33 @@ import pandas as pd
 from PIL import Image
 import time
 
-
+# -----------------------
+# タイトル
+# -----------------------
 st.title('Streamlit 超入門')
 
+# -----------------------
+# ヘッダー
+# -----------------------
+st.header('ヘッダー')
 
-st.write('DataFrame')
-df = pd.DataFrame({
-    '1列目': [1, 2, 3, 4],
-    '2列目': [10, 20, 30, 40]
-})
-st.dataframe(df.style.highlight_max(axis=0), width=500, height=300)
-st.table(df.style.highlight_max(axis=0))
+# -----------------------
+# サブヘッダー
+# -----------------------
+st.subheader('サブヘッダー')
 
-"""
+# -----------------------
+# Markdown記法
+# -----------------------
+md = """
+---
+**マークダウン**記法にも対応している。
 # header 1
 ## header 2
 ### header 3
+
+- List1
+- List2
 
 ```python
 import streamlit as st
@@ -29,6 +40,18 @@ import pandas as pd
 st.title('Streamlit 超入門')
 ```
 """
+st.markdown(md)
+
+# -----------------------
+# DataFrameと表、チャート
+# -----------------------
+st.header('DataFrame')
+df = pd.DataFrame({
+    '1列目': [1, 2, 3, 4],
+    '2列目': [10, 20, 30, 40]
+})
+st.dataframe(df.style.highlight_max(axis=0), width=500, height=300) # インタラクティブな表が作成可能（ソート可能）
+st.table(df.style.highlight_max(axis=0))                            # スタティックな表が作成可能（ソート不能）
 
 df2 = pd.DataFrame(
     np.random.rand(20, 3),
@@ -37,61 +60,72 @@ df2 = pd.DataFrame(
 st.line_chart(df2)
 st.area_chart(df2)
 
-"""
-## マップ
-"""
+# -----------------------
+# マップ
+# -----------------------
+st.header('Map')
 df3 = pd.DataFrame(
     np.random.rand(100, 2)/[50, 50] + [35.69, 139.70],
     columns=['lat', 'lon']
 )
 st.map(df3)
 
-"""
-## Display Image
-"""
+# -----------------------
+# 画像の表示
+# -----------------------
+st.header('Display Image')
 if st.checkbox('Show Image'):
-    img = Image.open('avatar.png')
+    img = Image.open('sample.jpg')
     st.image(img, caption='Sample', use_column_width=True)
 
-st.write('## Interactive widgets')
-st.sidebar.write('# Interactive widgets')
-option = st.sidebar.selectbox(
+# -----------------------
+# インタラクティブなウィジェット
+# -----------------------
+st.header('Interactive widgets')
+st.sidebar.header('Interactive widgets')
+num = st.sidebar.selectbox(
     '好きな数字は？',
     options=list(range(1, 11))
 )
-'あなたの好きな数字は、', option, 'ですね。'
-
+st.write('##### サイドバーの好きな数字を指定すると？')
+st.write('あなたの好きな数字は、', num, 'ですね。')
 
 input_txt = st.sidebar.text_input('あなたの趣味は？')
-'あなたの趣味は、', input_txt, 'ですね。'
+st.write('##### サイドバーの趣味を入力すると？')
+st.write('あなたの趣味は、', input_txt, 'ですね。')
 
 condition = st.sidebar.slider('あなたの今の調子は？', 0, 100, 50)
-'コンディション、', condition
+st.write('##### サイドバーの調子を調整すると？')
+st.write('コンディション、', condition)
 
-
+# -----------------------
+# カラム表示
+# -----------------------
 st.write('## Columns')
-left_column, right_cloumn = st.columns(2)
-button = left_column.button('右カラムに文字を表示')
-if button:
-    right_cloumn.write('ここに文字を書く')
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "25 °C", "3.0 °C")
+col2.metric("Wind", "9 m/s", "-8%")
+col3.metric("Humidity", "86%", "4%")
 
-
+# -----------------------
+# プログレスバー 
+# -----------------------
 st.write('## プログレスバー')
-'Start!!!'
-latest_iteration = st.empty()
-bar = st.progress(0)
+if st.button('Start Progress Bar'):
+    'Start!!!'
+    latest_iteration = st.empty()
+    bar = st.progress(0)
 
-for i in range(100):
-    latest_iteration.text(f'Iteration {i + 1}')
-    bar.progress(i + 1)
-    time.sleep(0.05)
-'Done!!!'
+    for i in range(100):
+        latest_iteration.text(f'Iteration {i + 1}')
+        bar.progress(i + 1)
+        time.sleep(0.05)
+    'Done!!!'
 
-
+# -----------------------
+# 拡張、縮小機能
+# -----------------------
 st.write('## Expander')
-expander = st.expander('問い合わせ')
-expander.write('問い合わせ内容を書く')
-expander.write('問い合わせ内容を書く')
-expander.write('問い合わせ内容を書く')
-expander.write('問い合わせ内容を書く')
-expander.write('問い合わせ内容を書く')
+expander = st.expander('Question')
+expander.write('Anser 1')
+expander.write('Anser 2')
